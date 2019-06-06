7a225d97186462a86b8150b3436c19b4c3ee42c0
Oshri <cms.x.seo@gmail.com>
2019-06-06 23:09:08 +0300

    Update angular json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular PWA on github pages';
+  title = 'Angular PWA deploy in github pages!!';
 }
