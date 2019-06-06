ab5acbba3c50725b25769b7756bcef585ff2761e
Oshri <cms.x.seo@gmail.com>
2019-06-06 22:30:35 +0300

    Add core module & swUpdates module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { SwUpdatesModule } from './modules/swUpdates';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwUpdatesModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,7 +56,8 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import '@webcomponents/custom-elements'; //  Custom Elements Polyfill
+import '@webcomponents/custom-elements/src/native-shim';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
